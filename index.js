fb7ae44b72c9627e8e3e9ecbc306c20c999b54c1
Vikas Patidar <patidarvikas50@gmail.com>
2025-05-14 16:48:26 +0530

    updates

--- index.js
+++ index.js
@@ -1,62 +1,84 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
 const users = require("./MOCK_DATA.json");
+const PORT = 8000;
 const fs = require("fs");
 
-
 // Middleware Pluggins---
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-// Routes
 app.get("/users", (req, res) => {
   const html = `
-        <ul>
-        ${users
-          .map((user, index) => `<li>${index + 1} ${user.first_name}</li>`)
-          .join("")}
-        </ul>
-        `;
-  res.send(html);
+  <ul>${users.map((user) => `<li>${user.first_name}</li>`).join("")}</ul>
+  `;
+  return res.send(html);
 });
 
-// REST API
-app.get("/api/users", (req, res) => {
-  return res.json(users);
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  const newUser = users.push({ ...body, id: users.length + 1 });
+
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ status: "Error", message: "Failed to save data" });
+    }
+    return res.json({ status: "Success", user: newUser, id: users.length });
+  });
 });
 
+// Routes
 app
   .route("/api/users/:id")
   .get((req, res) => {
-    // Get user with id
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
-  return  res.json(user);
+    return res.json(user);
   })
   .patch((req, res) => {
-    // Edit user with id
-    return res.json({ status: "pending" });
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user)=> user.id === id) 
+
+    if(userIndex === -1){
+      return res.status(404)
+      .json({status: "Error", message: "user not found"})
+    }
+    users[userIndex] = {...users[userIndex], ...body}
+
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+      if(err){
+        return res.status(505)
+        .json({status: "Error", message: "failed to update user"})
+      }{
+        return res.json({status: "success", user:users[userIndex]})
+      }
+    })
   })
   .delete((req, res) => {
-    // Delete user with id
-    return res.json({ status: "pending" });
-  });
-
-
-
-//    Create a new user using post method and postman 
-app.post("/api/users", (req, res) => {
+    const id = Number(req.params.id);
     const body = req.body;
-    const newUser = {...body, id: users.length +1}
-    users.push(newUser);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data)=>{
-        if (err) {
-            return res.status(500).json({ status: "Error", message: "Failed to save User" });
-           }
-           {
-            return res.json({ status: "Success", user: newUser, id: users.length });
-           }
-    })
-});
+    const userIndex = users.findIndex((user)=> user.id === id) 
 
-app.listen(PORT, () => console.log(`Server Strted at Port : ${PORT}`));
+    if(userIndex === -1){
+      return res.status(404)
+      .json({status: "Error", message: "user not found"})
+    }
+    users.splice(userIndex, 1)
+
+    users.forEach((user,index)=>{
+      user.id = index+1;
+    })
+
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+      if(err){
+        return res.status(505)
+        .json({status: "Error", message: "failed to Delete user"})
+      }{
+        return res.json({status: "success", user:users[userIndex]})
+      }
+    })
+  })
+
+app.listen(PORT, console.log("Server started port:", PORT));
